f4d477509601099182654850b404323d0f49aece
codeddave <drizzzydave@gmail.com>
2020-08-02 22:54:21 +0100

    updates

---
+++ src/components/menu-item/MenuItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MenuItem.scss"
+function MenuItem() {
+    return (
+        <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW!</span>
+        </div>
+      </div>
+    )
+}
+export default MenuItem
