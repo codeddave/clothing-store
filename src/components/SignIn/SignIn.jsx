68b8fd0b767f0857b59b0665a6766eb32fcebdf4
David <drizzzydave@gmail.com>
2020-08-05 04:21:52 +0100

    updated components

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -6,9 +6,54 @@ class SignIn extends Component {
     email: "",
     password: "",
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        console.log(this.state.email);
+      }
+    );
+  };
   render() {
-    return;
-    <div className="sign-in"></div>;
+    return (
+      <div className="sign-in">
+        <span>I already have an account</span>
+        <span> Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <label>Email</label>
+
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <label>Password</label>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 export default SignIn;
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./User.scss";
+import SignIn from "../SignIn/SignIn";
 
 function User() {
   return (
     <div>
       <h1>Sign In</h1>
+      <SignIn />
     </div>
   );
 }
