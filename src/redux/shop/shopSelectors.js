5c86a55c3bce4ba2f5016cc9b4d113f43252d1a9
Dvaid Adeleye <drizzzydave@gmail.com>
2020-11-14 10:46:42 -0800

    built WithSpinner HOC

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -18,5 +18,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = collectionUrlParam => 
     createSelector(
         [selectShopCollections], 
-        collections => collections ? collections[collectionUrlParam] : []
+        collections => collections ? collections[collectionUrlParam] : null
     )
