5313c30a3c35c7577e9b1c1b09679d9aa9bfa8bc
Dvaid Adeleye <drizzzydave@gmail.com>
2021-03-14 00:04:14 +0100

    updated fetchCollection Saga

--- src/redux/shop/shopSaga.js
+++ src/redux/shop/shopSaga.js
@@ -1,8 +1,23 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
 import { shopActionTypes } from "./shopActionTypes";
+import {
+  convertCollectionsSnapsotToMap,
+  firestore,
+} from "../../firebase/firebase.util";
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+} from "./shopActions";
 
 export function* fetchCollectionsAsync() {
-  yield console.log("I am fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapShot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionsSnapsotToMap, snapShot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStartSaga() {
